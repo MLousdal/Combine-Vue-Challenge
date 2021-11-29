a05afb4b183d7e71c4f378bb5cbbf6d16a658d36
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-11-29 22:05:48 +0100

    Components - styling
    Setup of project :30 mins
    App, Main, Header, Sidebar: 150 min
    
    Most of setup time was fiddling with codepen, and transitioning to vscode

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <Header></Header>
+  <header class="title">
+    <h1>Besøgsoplevelse</h1>
+    <p>Hvordan var din kundeoplevelse i butikken i dag?</p>
+  </header>
+  <Main></Main>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Lobster&family=Source+Sans+Pro:wght@400;600&display=swap");
+
+body {
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+}
+
+.wrapper {
+  max-width: 1240px;
+  margin: 0 auto;
+  width: 95%;
+}
+
+.title {
+  margin-top: 2rem;
+
+  h1 {
+    color: #149d2a;
+    font-size: 56px;
+    font-style: normal;
+    font-weight: bold;
+    line-height: 70px;
+    text-align: center;
+  }
+
+  p {
+    font-size: 22px;
+    line-height: 28px;
+    text-align: center;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <span class="logo">24Super</span>
+      <div class="header-right">
+        <address>Havnepladsen 50, 1234 Aalby</address>
+        <nav>
+          <a href="#">Forside</a>
+          <a href="#" class="active">Besøgsoplevelse</a>
+          <a href="#">Find butik</a>
+          <a href="#">Kontakt</a>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  background: linear-gradient(180deg, #118b24 0%, #149d2a 100%);
+  color: white;
+  padding: 1rem;
+
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    .header-right {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      align-items: flex-end;
+    }
+  }
+}
+
+.logo {
+  font-family: Lobster;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 60px;
+  line-height: 75px;
+}
+
+nav {
+  display: flex;
+  flex-direction: row;
+  gap: 3rem;
+
+  a {
+    font-family: Source Sans Pro;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 25px;
+    text-align: right;
+    text-decoration: none;
+    color: inherit;
+
+    &.active {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
